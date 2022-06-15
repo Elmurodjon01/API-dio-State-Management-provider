8eb8496acf5bcd664c4b15ebfb13aa7bc80ebcda
Elmurodjon <96878908+Elmurodjon01@users.noreply.github.com>
2022-06-15 18:18:50 +0900

    getting actual data through API, State Management(provider)

--- lib/providers/game_page_provider.dart
+++ lib/providers/game_page_provider.dart
@@ -30,14 +30,57 @@ class GamePageProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  String getCurrenQuestionText(){
+  String getCurrenQuestionText() {
     return questions![currentQuestionCount]['question'];
   }
 
   void answerQuestion(String _answer) async {
-    bool isCorrect = questions![currentQuestionCount]['correct_answer'] == _answer;
+    bool isCorrect =
+        questions![currentQuestionCount]['correct_answer'] == _answer;
     currentQuestionCount++;
-    print(isCorrect ? 'Correct' : 'Incorrect');
-    notifyListeners();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: isCorrect ? Colors.green : Colors.red,
+          title: Text(isCorrect ? 'Right' : 'Wrong'),
+          // Icon(
+          //   isCorrect ? Icons.circle : Icons.cancel_sharp,
+          //   color: Colors.white,
+          // ),
+        );
+      },
+    );
+    await Future.delayed(
+      const Duration(seconds: 1),
+    );
+    Navigator.pop(context);
+    if (currentQuestionCount == maxQuestions){
+      endGame();
+    }else{
+      notifyListeners();
+    }
+
+  }
+
+  Future<void> endGame() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const AlertDialog(
+          backgroundColor: Colors.blue,
+          title: Text(
+            'Game Ended!',
+            style: TextStyle(color: Colors.white, fontSize: 25),
+          ),
+          content: Text('Score: 0/0'),
+        );
+      },
+    );
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+    Navigator.pop(context);
+    Navigator.pop(context);
   }
 }
