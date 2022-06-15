24211bed68f3272a724ddb4bfbc769672c47ff5b
Elmurodjon <96878908+Elmurodjon01@users.noreply.github.com>
2022-06-15 17:28:27 +0900

    getting actual data through API, State Management(provider)

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '/pages/game_page.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Frivia',
+      theme: ThemeData(
+        fontFamily: "ArchitectsDaughter",
+        scaffoldBackgroundColor: const Color.fromRGBO(31, 31, 31, 1.0),
+        primarySwatch: Colors.blue,
+      ),
+      home: GamePage(),
+    );
+  }
+
+
+}
